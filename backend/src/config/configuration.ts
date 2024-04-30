a4e3333075cde87b8369f62d41fb1bc62c2019ab
Dmitry Makarov <makdmt@gmail.com>
2024-04-30 12:46:03 +0300

    feat (app) add base Dockerfiles and docker-compose file

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,14 +1,14 @@
 
 export default () => ({
     server: {
-        port: parseInt(process.env.SERVER_PORT) || 3000
+        port: parseInt(process.env.NEST_PORT) || 3000
     },
     database: {
-        host: process.env.DB_HOST || '127.0.0.1',
-        port: parseInt(process.env.DB_PORT) || 5432,
-        user: process.env.DB_USER || 'student',
-        password: process.env.DB_PASSWORD || 'student',
-        name: process.env.DB_NAME || 'kupipodariday',
+        host: process.env.POSTGRES_HOST || '127.0.0.1',
+        port: parseInt(process.env.POSTGRES_PORT) || 5432,
+        user: process.env.POSTGRES_USER || 'student',
+        password: process.env.POSTGRES_PASSWORD || 'student',
+        name: process.env.POSTGRES_DB || 'kupipodariday',
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'secret-key',
